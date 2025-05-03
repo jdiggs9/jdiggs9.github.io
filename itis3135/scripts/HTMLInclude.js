a14cb658addfb2068579eb558e7c2d43983a3033
jdiggs9 <160246790+jdiggs9@users.noreply.github.com>
2025-05-03 14:25:47 -0400

    Dynamic Headers/Footers

---
+++ itis3135/scripts/HTMLInclude.js
@@ -0,0 +1,17 @@
+function includeHTML() {
+    const elements = document.querySelectorAll('[data-include]');
+  
+    elements.forEach(el => {
+      const file = el.getAttribute('data-include');
+      fetch(file)
+        .then(response => {
+          if (!response.ok) throw new Error(`Failed to fetch ${file}`);
+          return response.text();
+        })
+        .then(data => el.innerHTML = data)
+        .catch(error => console.error(error));
+    });
+  }
+  
+  window.addEventListener('DOMContentLoaded', includeHTML);
+  
